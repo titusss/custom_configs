9c43f18bc328afd0a107823be89ce6daed8dbd57
titusss <26855197+titusss@users.noreply.github.com>
2022-08-24 19:02:48 -0700

    updated cam

--- 220824_depth_formating.py
+++ 220824_depth_formating.py
@@ -200,9 +200,9 @@ class Collect(object):
         data = {}
         img_meta = {}
         results['cam_intrinsic'] = \
-            [[5.1885790117450188e+02, 0, 3.2558244941119034e+02],
-             [5.1946961112127485e+02, 0, 2.5373616633400465e+02],
-             [0                     , 0, 1                    ]]
+            [[0, 0, 0],
+             [0, 0, 0],
+             [0, 0, 2]]
         for key in self.meta_keys:
             img_meta[key] = results[key]
         data['img_metas'] = DC(img_meta, cpu_only=True)
